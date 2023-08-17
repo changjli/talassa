59e41b51c9d525a0b68b878482310da9ee78de05
Nicholas Audric Surya <100661447+changjli@users.noreply.github.com>
2023-08-17 15:22:58 +0700

    sfsdf

---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <nav class="navbar">
+                <img src="assets/images/Logo.png" alt="" class="Logo" />
+
+                <ul class="nav-menu">
+                    <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Group</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">About Us</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Support</a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
