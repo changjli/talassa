fdef86760b65df4433691bc8ec914ece6a67df27
Nicholas Audric Surya <100661447+changjli@users.noreply.github.com>
2023-08-25 20:51:24 +0700

    f

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,17 @@
 const Navbar = () => {
     return (
-        <nav class="navbar">
-            <img src="assets/images/Logo.png" alt="" class="Logo" />
+        <div className="navbar">
+            <nav class="navbar">
+                <img src="assets/images/Logo.png" alt="" class="Logo" />
 
-            <ul class="nav-menu">
-                <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Group</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">About Us</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Support</a></li>
-            </ul>
-        </nav>
+                <ul class="nav-menu">
+                    <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Group</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">About Us</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Support</a></li>
+                </ul>
+            </nav>
+        </div>
     );
 }
 
